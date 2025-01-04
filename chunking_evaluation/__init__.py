ca659de8849b9f4002a7ef38a4f1347e62b99c5a
Oleksii Horhcynskyi <pan.alexeu@gmail.com>
2025-01-04 16:55:09 +0200

    exported DatasetEvaluation in chunking_evaluation module

--- chunking_evaluation/__init__.py
+++ chunking_evaluation/__init__.py
@@ -1,10 +1,12 @@
 from .chunking.base_chunker import BaseChunker
 from .evaluation_framework.general_evaluation import GeneralEvaluation
 from .evaluation_framework.synthetic_evaluation import SyntheticEvaluation
+from .evaluation_framework.dataset_evaluation import DatasetEvaluation
 from .utils import *
 
 __all__ = [
     'BaseChunker',
     'GeneralEvaluation',
     'SyntheticEvaluation',
+    'DatasetEvaluation'
 ]
